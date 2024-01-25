8535ca6ef2d4915e818b336346892a5cc183d5d7
cocoluo <cocoluo@localhost>
2024-01-25 11:13:45 -0600

    sutdio 1

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,6 +15,12 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
+		
+		boolean descending = (x > y) && (y> z); 
+		boolean ascending = (x < y) && (y < z);
+		boolean isOrdered = descending || ascending; 
+		
+		System.out.println(isOrdered);
 
 	}
 
